69df2b4b06cc5d1a44652b766a0e5bdb95b03ea2
philiplam <74084724+philiplamscript@users.noreply.github.com>
2025-12-23 23:20:21 +0800

    update

--- Gambling_simulate/Homepage.py
+++ Gambling_simulate/Homepage.py
@@ -14,12 +14,12 @@ import os
 # Get the current directory (main)
 current_dir = os.getcwd()
 
-# Join with your target folder
-target_dir = os.path.join(current_dir, "Gambling_simulate")
+# # Join with your target folder
+# target_dir = os.path.join(current_dir, "Gambling_simulate")
 
 # Change the working directory
-if os.path.exists(target_dir):
-    os.chdir(target_dir)
+if os.path.exists(current_dir):
+    os.chdir(current_dir)
 
 # Now, any relative paths will start inside /folder1
 print(f"Current working directory: {os.getcwd()}")
